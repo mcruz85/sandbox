b137fcb3b6cc8a1f860a0779b590b4219161e254
ednardorubens <ednardorubens@hotmail.com>
2017-10-13 11:15:05 -0300

    Limpando Espaços

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -26,15 +26,12 @@ module.exports = function() {
 	  console.log('fim');
 
 	  var contato = contatos.filter((contato) => {
-	    return contato._id == id;		
+	    return contato._id == id;
 	  })[0];
 
 	  contato ?
 	    res.json(contato) :
 	    res.status(404).send('Contato não encontrado');
-
-
-
 	};
 
 	return controller;
